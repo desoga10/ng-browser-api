53edd36b59c9f91f7c867fc7b49e4e0ca61231cc
desoga10 <done2stay@gmail.com>
2022-05-09 00:23:25 +0100

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Self, SkipSelf } from '@angular/core';
+import { BROWSER_STORAGE, StorageService } from './storage.service';
 
 @Component({
   selector: 'app-root',
+  providers: [
+    StorageService,
+    { provide: BROWSER_STORAGE, useFactory: () => sessionStorage },
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'browser';
+
+  constructor(
+    @Self() private sessionStorageService: StorageService,
+    @SkipSelf() private localStorageService: StorageService
+  ) {}
+
+  ngOnInit() {}
+
+  setSession() {
+    this.sessionStorageService.set('hero', 'Dr Nice - Session');
+  }
+
+  clearSession() {
+    this.sessionStorageService.removeSession();
+  }
+
+  setLocal() {
+    this.localStorageService.set('hero', 'Dr Nice - Local');
+  }
+
+  clearLocal() {
+    this.localStorageService.removeLocal();
+  }
 }
